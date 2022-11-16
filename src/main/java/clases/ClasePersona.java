e4f14440b3235c8549317c8363b8a9eb864ddc6f
Noelia <npergon914@g.educaand.es>
2022-11-16 20:15:34 +0100

    medio hecho

--- src/main/java/clases/ClasePersona.java
+++ src/main/java/clases/ClasePersona.java
@@ -4,30 +4,69 @@
  */
 package clases;
 
+import java.util.Random;
+
 /**
  *
  * @author noelia
  */
 public class ClasePersona {
-    
+
     String nombre;
-    String edad;
+    int edad;
     String nif;
-    String sexo;
+    char sexo;
     double peso;
     double altura;
 
     public ClasePersona() {
     }
 
-    public ClasePersona(String nombre, String edad, String sexo, double peso, double altura) {
+    public ClasePersona(String nombre, int edad, char sexo, double peso, double altura) {
         this.nombre = nombre;
         this.edad = edad;
-        this.sexo = sexo;
         this.peso = peso;
         this.altura = altura;
+        if (filtrarSexo(sexo)) {
+            this.sexo = sexo;
+        } else {
+            this.sexo = 'O';
+        }
+        this.nif = generaNIF();
+    }
+
+    public static boolean filtrarSexo(char sexo) {
+
+        if (sexo == 'H' || sexo == 'M' || sexo == 'O') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String generaNIF() {
+        String numero = "";
+
+        Random aleatorio = new Random();
+        for (int i = 0; i <= 8; i++) {   
+            numero = numero + Integer.toString(aleatorio.nextInt(10));
+        }
+        
+        int resto = Integer.parseInt(numero) % 23;
+        String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+        numero = numero + letras.charAt(resto);
+        
+        return numero;
+    }
+    
+    public boolean esMayorEdad(){
+        if (this.edad >= 18){
+            return true;
+        } else {
+            return false;
+        }
     }
     
     
-    
+
 }
