ee7013bf7c6585af43cd50ca0c012cb5540ca3a5
Noelia <perezgonzaleznoelia08@gmail.com>
2022-11-16 14:39:45 +0100

    empezao

--- src/main/java/clases/ClasePersona.java
+++ src/main/java/clases/ClasePersona.java
@@ -16,5 +16,18 @@ public class ClasePersona {
     String sexo;
     double peso;
     double altura;
+
+    public ClasePersona() {
+    }
+
+    public ClasePersona(String nombre, String edad, String sexo, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+    }
+    
+    
     
 }
